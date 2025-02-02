bda2c30d66033aa8cc451a2ea6ce42303579b2ec
sanjaybora04 <sanjaybora380@gmail.com>
2025-02-02 17:20:08 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,9 @@ export default function Home() {
   return (
     <div className="max-w-3xl mx-auto flex flex-col min-h-svh px-4 py-8 gap-8">
       <header className="flex flex-col gap-1">
-        <h1 className="text-3xl font-bold tracking-tight">Custom Registry</h1>
+        <h1 className="text-3xl font-bold tracking-tight">Advanced Shadcn Components</h1>
         <p className="text-muted-foreground">
-          A custom registry for distribution code using shadcn.
+          A registry for advance shadcn components
         </p>
       </header>
       <main className="flex flex-col flex-1 gap-8">

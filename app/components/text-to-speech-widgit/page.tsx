949dd43197f6d72e61e6715365b7e13d486c0b1e
sanjaybora04 <sanjaybora380@gmail.com>
2025-02-13 19:41:38 +0530

    commit

--- app/components/text-to-speech-widgit/page.tsx
+++ app/components/text-to-speech-widgit/page.tsx
@@ -1,4 +1,10 @@
 import TextToSpeech from "@/components/text-to-speech";
+import { Metadata } from "next";
+
+export const metadata:Metadata = {
+    title: "Shadcn Text to speech widgit",
+    description: "A text to speech widgit for reading blogs and article pages"
+}
 
 export default function Page(){
     return (
